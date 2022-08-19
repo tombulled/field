b4d238a4f43dd99a842ae302c720f975a48954c5
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-08-19 13:58:10 +0100

    Make `models.Arguments` frozen

--- param/models.py
+++ param/models.py
@@ -7,7 +7,7 @@ from .sentinels import Missing, MissingType
 T = TypeVar("T")
 
 
-@dataclass(frozen=True, eq=True)
+@dataclass(frozen=True)
 class ParameterSpecification(Generic[T]):
     default: Union[T, MissingType] = Missing
     default_factory: Union[Callable[[], T], MissingType] = Missing
@@ -28,14 +28,14 @@ class ParameterSpecification(Generic[T]):
         return self.default is not Missing or self.default_factory is not Missing
 
 
-@dataclass
+@dataclass(frozen=True)
 class Arguments:
     args: Tuple[Any, ...] = field(default_factory=tuple)
     kwargs: Dict[str, Any] = field(default_factory=dict)
     arguments: Dict[str, Any] = field(default_factory=dict)
 
 
-@dataclass(frozen=True, eq=True)
+@dataclass(frozen=True)
 class Parameter:
     name: str
     annotation: Any
