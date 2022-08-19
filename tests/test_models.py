bbb06e830a27e612171e99f61c445143bcdd12e0
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-08-19 13:54:31 +0100

    Fix tests to use new `ParameterSpecification.get_default()` method

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,15 +3,15 @@ from param import ParameterSpecification, Missing
 
 
 def test_param_spec_no_default() -> None:
-    assert ParameterSpecification().default is Missing
+    assert ParameterSpecification().get_default() is Missing
 
 
 def test_param_spec_default() -> None:
-    assert ParameterSpecification(default=123).default == 123
+    assert ParameterSpecification(default=123).get_default() == 123
 
 
 def test_param_spec_default_factory() -> None:
-    assert ParameterSpecification(default_factory=dict).default == {}
+    assert ParameterSpecification(default_factory=dict).get_default() == {}
 
 
 def test_param_spec_default_and_default_factory() -> None:
