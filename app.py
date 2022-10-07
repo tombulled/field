1e84ae65757f603c60a5bdbc27d9907a5eec4ae8
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-10-07 21:04:28 +0100

    Test a class-aware decorator

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from param import Param, params
+from param import Param, params, get_params
+from abc import ABC
 
 @params
 def get(url: str, params: dict = Param(default_factory=dict)):
     print("GET", url, params)
 
-class MyClass:
+class MyClass(ABC):
     @params
     def get(self, url: str, params: dict = Param(default_factory=dict)):
         print("GET", url, params)
 
 get("https://httpbin.com/get")
+MyClass.get("self here", "https://httpbin.com/get")
 MyClass().get("https://httpbin.com/get")
