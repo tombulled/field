fb7a1402197ed761e8047d0a7be082f361c402da
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-10-15 14:35:50 +0100

    Make `parameters.Param` frozen

--- param/parameters.py
+++ param/parameters.py
@@ -17,7 +17,7 @@ class BaseParam(FieldInfo):
             return self.default
 
 
-@dataclass(init=False)
+@dataclass(frozen=True)
 class Param(BaseParam):
     default: Union[Any, UndefinedType] = Undefined
     default_factory: Optional[Supplier[Any]] = None
@@ -45,62 +45,3 @@ class Param(BaseParam):
     discriminator: Optional[str] = None
     repr: bool = True
     extra: Dict[str, Any] = field(default_factory=dict)
-
-    def __init__(
-        self,
-        default: Union[Any, UndefinedType] = Undefined,
-        *,
-        default_factory: Optional[Supplier[Any]] = None,
-        alias: Optional[str] = None,
-        title: Optional[str] = None,
-        description: Optional[str] = None,
-        exclude: Union[Set[Union[int, str]], Mapping[Union[int, str], Any], Any] = None,
-        include: Union[Set[Union[int, str]], Mapping[Union[int, str], Any], Any] = None,
-        const: Optional[bool] = None,
-        gt: Optional[float] = None,
-        ge: Optional[float] = None,
-        lt: Optional[float] = None,
-        le: Optional[float] = None,
-        multiple_of: Optional[float] = None,
-        allow_inf_nan: Optional[bool] = None,
-        max_digits: Optional[int] = None,
-        decimal_places: Optional[int] = None,
-        min_items: Optional[int] = None,
-        max_items: Optional[int] = None,
-        unique_items: Optional[bool] = None,
-        min_length: Optional[int] = None,
-        max_length: Optional[int] = None,
-        allow_mutation: bool = True,
-        regex: Optional[str] = None,
-        discriminator: Optional[str] = None,
-        repr: bool = True,
-        **extra: Any,
-    ):
-        super().__init__(
-            default,
-            default_factory=default_factory,
-            alias=alias,
-            title=title,
-            description=description,
-            exclude=exclude,
-            include=include,
-            const=const,
-            gt=gt,
-            ge=ge,
-            lt=lt,
-            le=le,
-            multiple_of=multiple_of,
-            allow_inf_nan=allow_inf_nan,
-            max_digits=max_digits,
-            decimal_places=decimal_places,
-            min_items=min_items,
-            max_items=max_items,
-            unique_items=unique_items,
-            min_length=min_length,
-            max_length=max_length,
-            allow_mutation=allow_mutation,
-            regex=regex,
-            discriminator=discriminator,
-            repr=repr,
-            **extra,
-        )
