e1501248ab5fced1871ada9adca88650c51cf802
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-10-15 14:23:08 +0100

    Add defaults for `parameters.Param`

--- param/parameters.py
+++ param/parameters.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Dict, Mapping, Optional, Set, Union
 
 from pydantic.fields import FieldInfo, Undefined, UndefinedType
@@ -19,32 +19,32 @@ class BaseParam(FieldInfo):
 
 @dataclass(init=False)
 class Param(BaseParam):
-    default: Union[Any, UndefinedType]
-    default_factory: Optional[Supplier[Any]]
-    alias: Optional[str]
-    title: Optional[str]
-    description: Optional[str]
-    exclude: Union[Set[Union[int, str]], Mapping[Union[int, str], Any], Any]
-    include: Union[Set[Union[int, str]], Mapping[Union[int, str], Any], Any]
-    const: Optional[bool]
-    gt: Optional[float]
-    ge: Optional[float]
-    lt: Optional[float]
-    le: Optional[float]
-    multiple_of: Optional[float]
-    allow_inf_nan: Optional[bool]
-    max_digits: Optional[int]
-    decimal_places: Optional[int]
-    min_items: Optional[int]
-    max_items: Optional[int]
-    unique_items: Optional[bool]
-    min_length: Optional[int]
-    max_length: Optional[int]
-    allow_mutation: bool
-    regex: Optional[str]
-    discriminator: Optional[str]
-    repr: bool
-    extra: Dict[str, Any]
+    default: Union[Any, UndefinedType] = Undefined
+    default_factory: Optional[Supplier[Any]] = None
+    alias: Optional[str] = None
+    title: Optional[str] = None
+    description: Optional[str] = None
+    exclude: Union[Set[Union[int, str]], Mapping[Union[int, str], Any], Any] = None
+    include: Union[Set[Union[int, str]], Mapping[Union[int, str], Any], Any] = None
+    const: Optional[bool] = None
+    gt: Optional[float] = None
+    ge: Optional[float] = None
+    lt: Optional[float] = None
+    le: Optional[float] = None
+    multiple_of: Optional[float] = None
+    allow_inf_nan: Optional[bool] = None
+    max_digits: Optional[int] = None
+    decimal_places: Optional[int] = None
+    min_items: Optional[int] = None
+    max_items: Optional[int] = None
+    unique_items: Optional[bool] = None
+    min_length: Optional[int] = None
+    max_length: Optional[int] = None
+    allow_mutation: bool = True
+    regex: Optional[str] = None
+    discriminator: Optional[str] = None
+    repr: bool = True
+    extra: Dict[str, Any] = field(default_factory=dict)
 
     def __init__(
         self,
