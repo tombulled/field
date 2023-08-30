9b35e3fa8674b780fa043e97e7e28486c53de7bb
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-08-30 21:51:08 +0100

    Fix models tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,33 +1,7 @@
 import inspect
-from typing import Any
-
-from arguments import Arguments, BoundArguments
 
 from param.enums import ParameterType
 from param.models import Parameter
-
-
-def test_arguments() -> None:
-    def captor(*args: Any, **kwargs: Any) -> Arguments:
-        return Arguments(args=args, kwargs=kwargs)
-
-    arguments: Arguments = Arguments(args=("foo",), kwargs={"bar": "bar"})
-
-    assert arguments.call(captor) == arguments
-
-
-def test_bound_arguments() -> None:
-    def captor(name: str, age: int = 40) -> dict:
-        return dict(name=name, age=age)
-
-    assert BoundArguments(args={"name": "sam"}, kwargs={}).call(captor) == {
-        "name": "sam",
-        "age": 40,
-    }
-    assert BoundArguments(args={"name": "john"}, kwargs={"age": 10}).call(captor) == {
-        "name": "john",
-        "age": 10,
-    }
 
 
 def test_parameter_from_parameter() -> None:
