6dbf8d8d404c216ddfa8ec2884b437e8f192e678
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-09-14 22:34:50 +0100

    [pydantic-v2] Remove app and tst files

--- app.pydantic-v2.py
+++
@@ -1,9 +0,0 @@
-from pydantic import Field
-from pydantic.fields import FieldInfo
-from typing_extensions import Annotated
-
-from param.enums import ParameterType
-
-
-def say(message: Annotated[str, Field(default="Hello, World!")]) -> None:
-    print(message)
--- tst.pydantic-root-model.py
+++
@@ -1,4 +0,0 @@
-from pydantic import Field, RootModel
-from typing_extensions import Annotated
-
-Name = RootModel[Annotated[str, Field(default="sally", max_length=3)]]
