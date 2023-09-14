f4d9b8d5f45d65cc93e6d518dae8d2275e53719c
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-09-14 22:30:14 +0100

    [pydantic-v2] Test .resolver

---
+++ tests/test_resolver.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+from param import Resolver
+
+
+def test_Resolver_isinstance() -> None:
+    class Metadata:
+        pass
+
+    def my_resolver(metadata: Metadata, argument: Any) -> str:
+        return "yo!"
+    
+    assert isinstance(my_resolver, Resolver)
