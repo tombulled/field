102bf186716a53d47212fc7fc44d4c3f4463ba2f
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-09-14 22:23:30 +0100

    [pydantic-v2] Remove all previous tests

--- tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-from inspect import Parameter
-
-from param import utils
-from param.sentinels import Undefined
-
-
-def test_parse():
-    assert utils.empty_to_missing(Parameter.empty) is Undefined
-    assert utils.empty_to_missing(123) == 123
