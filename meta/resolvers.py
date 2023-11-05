d83e7113c7b58949ab41149cab9d50bdd8bd18e8
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-11-05 22:51:07 +0000

    [pydantic-v2] Tidy up resolver exception messages

--- meta/resolvers.py
+++ meta/resolvers.py
@@ -23,23 +23,16 @@ from .protocols import (
 
 T = TypeVar("T")
 
-# TODO: Rename `resolve` to one of:
-#   apply / validate / implement / check / handle / consume / parse?
-
 
 def resolve_gt(meta: Gt, value: T) -> T:
     gt: SupportsGt = meta.gt
 
     if not isinstance(value, SupportsGt):
-        raise TypeError(f"The {Gt.__name__!r} constraint cannot be applied to {type(value)}")
+        raise TypeError(
+            f"The {Gt.__name__!r} constraint cannot be applied to {type(value)}"
+        )
 
     if not value > gt:
-        # raise ValueError(f"Input should be greater than {gt}")
-        # raise ValueError(f"Failed constraint: value > {gt}, where value = {value}")
-        # raise ValueError(f"Failed constraint: value > {gt} (value = {value})")
-        # raise ValueError(f"Value must be greater than {gt}. Got {value}")
-        # raise ValueError(f"Value must be greater than {gt} (got {value})")
-        # raise ValueError(f"Value must be greater than {gt} ({value} <= {gt})")
         raise ValueError(f"Value {value} not greater than {gt}")
 
     return value
@@ -49,11 +42,11 @@ def resolve_ge(meta: Ge, value: T) -> T:
     ge: SupportsGe = meta.ge
 
     if not isinstance(value, SupportsGe):
-        raise TypeError(f"The 'ge' constraint cannot be applied to {type(value)}")
+        raise TypeError(
+            f"The {Ge.__name__!r} constraint cannot be applied to {type(value)}"
+        )
 
     if not value >= ge:
-        # raise ValueError(f"Input should be greater than or equal to {ge}")
-        # raise ValueError(f"Value {value} not >= {ge}")
         raise ValueError(f"Value {value} not greater than or equal to {ge}")
 
     return value
@@ -63,10 +56,11 @@ def resolve_lt(meta: Lt, value: T) -> T:
     lt: SupportsLt = meta.lt
 
     if not isinstance(value, SupportsLt):
-        raise TypeError(f"The 'lt' constraint cannot be applied to {type(value)}")
+        raise TypeError(
+            f"The {Lt.__name__!r} constraint cannot be applied to {type(value)}"
+        )
 
     if not value < lt:
-        # raise ValueError(f"Input should be less than {lt}")
         raise ValueError(f"Value {value} not less than {lt}")
 
     return value
@@ -76,10 +70,12 @@ def resolve_le(meta: Le, value: T) -> T:
     le: SupportsLe = meta.le
 
     if not isinstance(value, SupportsLe):
-        raise TypeError(f"The 'le' constraint cannot be applied to {type(value)}")
+        raise TypeError(
+            f"The {Le.__name__!r} constraint cannot be applied to {type(value)}"
+        )
 
     if not value <= le:
-        raise ValueError(f"Input should be less than {le}")
+        raise ValueError(f"Value {value} not less than or equal to {le}")
 
     return value
 
@@ -102,20 +98,19 @@ def resolve_multiple_of(meta: MultipleOf, value: T) -> T:
 
         return value
     else:
-        raise TypeError(f"Invalid `multiple_of` value of type {type(value)}")
+        raise TypeError(f"Invalid `multiple_of` {type(value)}")
 
 
 def resolve_min_len(meta: MinLen, value: T) -> T:
     min_length: int = meta.min_length
 
     if not isinstance(value, Sized):
-        # raise TypeError(f"Input should implement {Sized}")
         raise TypeError(
-            f"Function {len.__name__!r} cannot be applied to type {type(value)} (must implement {Sized})"
+            f"The {MinLen.__name__!r} constraint cannot be applied to {type(value)}"
         )
 
     if not len(value) >= min_length:
-        raise ValueError(f"Length {len(value)} less than {min_length}")
+        raise ValueError(f"Value length {len(value)} less than {min_length}")
 
     return value
 
@@ -124,10 +119,12 @@ def resolve_max_len(meta: MaxLen, value: T) -> T:
     max_length: int = meta.max_length
 
     if not isinstance(value, Sized):
-        raise TypeError(f"Input should implement {Sized}")
+        raise TypeError(
+            f"The {MaxLen.__name__!r} constraint cannot be applied to {type(value)}"
+        )
 
     if not len(value) <= max_length:
-        raise ValueError(f"Length {len(value)} greater than {max_length}")
+        raise ValueError(f"Value length {len(value)} greater than {max_length}")
 
     return value
 
@@ -138,7 +135,7 @@ def resolve_timezone(meta: Timezone, value: T) -> T:
 
 def resolve_predicate(meta: Predicate, value: T) -> T:
     predicate: Callable[[Any], bool] = meta.func
-    
+
     if not predicate(value):
         raise ValueError(f"Value {value!r} failed predicate {predicate!r}")
 
