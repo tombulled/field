56950d1fe3288eca3bc7922305b309ab1a0e1507
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-03-02 01:00:35 +0000

    create baseresolvergroup

--- meta/group.py
+++ meta/group.py
@@ -1,39 +1,49 @@
+from abc import abstractmethod
 from typing import Any, Dict, Mapping, Optional, Type, TypeVar
 
 from meta.api import Resolver
 from meta.errors import ResolutionError
 
+K = TypeVar("K")
 M = TypeVar("M")
 R = TypeVar("R")
 
 
-class LiteralResolverGroupABC(Mapping[M, Resolver[M, R]], Resolver[M, R]):
+class BaseResolverGroup(Mapping[K, Resolver[M, R]], Resolver[M, R]):
     def __call__(self, metadata: M, value: Any) -> R:
-        resolver: Optional[Resolver[M, R]] = self.get(metadata)
+        return self.resolve(metadata, value)
+
+    def resolve(self, metadata: M, value: Any) -> R:
+        resolver: Optional[Resolver[M, R]] = self.get_resolver(metadata)
 
         if resolver is None:
-            raise ResolutionError
+            raise ResolutionError(f"No resolver available for metadata {metadata!r}")
 
         return resolver(metadata, value)
+
+    @abstractmethod
+    def get_resolver(self, metadata: M, /) -> Optional[Resolver[M, R]]:
+        raise NotImplementedError
+
+    def can_resolve(self, metadata: M, /) -> bool:
+        return self.get_resolver(metadata) is not None
+
+
+class LiteralResolverGroupABC(BaseResolverGroup[M, M, R]):
+    def get_resolver(self, metadata: M, /) -> Optional[Resolver[M, R]]:
+        return self.get(metadata)
 
 
 class LiteralResolverGroup(Dict[M, Resolver[M, R]], LiteralResolverGroupABC[M, R]):
     pass
 
 
-class ResolverGroupABC(Mapping[Type[M], Resolver[M, R]], Resolver[M, R]):
-    def __call__(self, metadata: M, value: Any) -> R:
-        resolver: Optional[Resolver[M, R]] = self._get_resolver(metadata)
-
-        if resolver is None:
-            raise ResolutionError
-
-        return resolver(metadata, value)
-
-    def _get_resolver(self, metadata: M, /) -> Optional[Resolver[M, R]]:
-        for cls in type(metadata).mro():
-            if cls in self:
-                return self[cls]
+class ResolverGroupABC(BaseResolverGroup[Type[M], M, R]):
+    def get_resolver(self, metadata: M, /) -> Optional[Resolver[M, R]]:
+        typ: type
+        for typ in type(metadata).mro():
+            if typ in self:
+                return self[typ]
 
         return None
 
--- meta/utils.py
+++
@@ -1,5 +0,0 @@
-def get_closest_subclass(typ, types):
-    for cls in typ.mro():
-        if cls in types:
-            return cls
-    return None
